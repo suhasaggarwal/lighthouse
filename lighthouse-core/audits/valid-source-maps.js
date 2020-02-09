39b744c0330350dd0c15f51c2a3cd1b9f28ce905
Connor Clark <cjamcl@google.com>
2020-02-08 21:30:48 -0800

    valid source maps

--- lighthouse-core/audits/valid-source-maps.js
+++ lighthouse-core/audits/valid-source-maps.js
@@ -5,7 +5,6 @@
  */
 'use strict';
 
-const thirdPartyWeb = require('third-party-web/httparchive-nostats-subset');
 const Audit = require('./audit.js');
 const i18n = require('../lib/i18n/i18n.js');
 
@@ -14,26 +13,12 @@ const UIStrings = {
   /** Title of a Lighthouse audit that provides detail on HTTP to HTTPS redirects. This descriptive title is shown to users when HTTP traffic is redirected to HTTPS. */
   title: 'Page has valid source maps',
   /** Title of a Lighthouse audit that provides detail on HTTP to HTTPS redirects. This descriptive title is shown to users when HTTP traffic is not redirected to HTTPS. */
-  failureTitle: 'Missing source maps for large first party JavaScript',
+  failureTitle: 'Has invalid source maps',
   /** Description of a Lighthouse audit that tells the user that their JavaScript source maps are invalid or missing. This is displayed after a user expands the section to see more. No character length limits. 'Learn More' becomes link text to additional documentation. */
   description: 'your maps are sad. [Learn more](https://web.dev/valid-source-maps).',
 };
 
 const str_ = i18n.createMessageInstanceIdFn(__filename, UIStrings);
-
-/**
- * @param {string} url
- * @param {string} finalUrl
- */
-function isFirstParty(url, finalUrl) {
-  try {
-    const entity = thirdPartyWeb.getEntity(url);
-    if (!entity) return true;
-    return entity === thirdPartyWeb.getEntity(finalUrl);
-  } catch (_) {
-    return false;
-  }
-}
 
 class ValidSourceMaps extends Audit {
   /**
@@ -45,7 +30,7 @@ class ValidSourceMaps extends Audit {
       title: str_(UIStrings.title),
       failureTitle: str_(UIStrings.failureTitle),
       description: str_(UIStrings.description),
-      requiredArtifacts: ['ScriptElements', 'SourceMaps', 'URL'],
+      requiredArtifacts: ['SourceMaps'],
     };
   }
 
@@ -56,82 +41,73 @@ class ValidSourceMaps extends Audit {
   static audit(artifacts) {
     const {SourceMaps} = artifacts;
 
-    /** @type {Set<string>} */
-    const isMissingMapForLargeFirstPartyScriptUrl = new Set();
-
-    let missingMapsForLargeFirstPartyFile = false;
     const results = [];
-    for (const ScriptElement of artifacts.ScriptElements) {
-      if (!ScriptElement.src) continue; // TODO: inline scripts, how do they work?
+    for (const sourceMapOrError of SourceMaps) {
+      const {scriptUrl, sourceMapUrl} = sourceMapOrError;
 
-      const SourceMap = SourceMaps.find(m => m.scriptUrl === ScriptElement.src);
-      const errors = [];
-      const isLargeFirstParty =
-        ScriptElement.content && ScriptElement.content.length >= 500 * 1024
-          && isFirstParty(ScriptElement.src, artifacts.URL.finalUrl);
-
-      if (isLargeFirstParty && (!SourceMap || !SourceMap.map)) {
-        missingMapsForLargeFirstPartyFile = true;
-        isMissingMapForLargeFirstPartyScriptUrl.add(ScriptElement.src);
-        errors.push('Large JavaScript file is missing a source map');
+      // Load errors.
+      if (!sourceMapOrError.map) {
+        const error = sourceMapOrError.errorMessage;
+        results.push({
+          scriptUrl,
+          sourceMapUrl,
+          error,
+        });
+        continue;
       }
 
-      if (SourceMap && !SourceMap.map) {
-        errors.push(SourceMap.errorMessage);
-      }
-
-      // Sources content errors.
-      if (SourceMap && SourceMap.map) {
-        const sourcesContent = SourceMap.map.sourcesContent || [];
-        let missingSourcesContentCount = 0;
-        for (let i = 0; i < SourceMap.map.sources.length; i++) {
-          if (sourcesContent.length < i || !sourcesContent[i]) missingSourcesContentCount += 1;
-        }
-        if (missingSourcesContentCount > 0) {
-          errors.push(`missing ${missingSourcesContentCount} items in \`.sourcesContent\``);
-        }
-      }
+      const map = sourceMapOrError.map;
 
       // TODO(cjamcl) validate (maybe source-map-validator) the map. Can punt this until maps
-      // are used for mapping in the report (we'd show a snippet of source code, or
+      // are used for mapping in the report (we'd show a snippet of source code, or 
       // show a source position instead of generated position).
 
-      if (SourceMap || errors.length) {
-        results.push({
-          scriptUrl: ScriptElement.src,
-          sourceMapUrl: SourceMap && SourceMap.sourceMapUrl,
-          errors,
-        });
+      // Sources content errors.
+      const sourcesContent = map.sourcesContent || [];
+      let missingSourcesContentCount = 0;
+      for (let i = 0; i < map.sources.length; i++) {
+        if (sourcesContent.length < i || !sourcesContent[i]) missingSourcesContentCount += 1;
       }
+      if (missingSourcesContentCount > 0) {
+        results.push({
+          scriptUrl: scriptUrl,
+          sourceMapUrl: sourceMapUrl,
+          error: `missing ${missingSourcesContentCount} items in \`.sourcesContent\``,
+        });
+        continue;
+      }
+
+      results.push({
+        scriptUrl: scriptUrl,
+        sourceMapUrl: sourceMapUrl,
+      });
+    }
+
+    // TODO(cjamcl) find any script urls that look like bundled code but don't have
+    // a source map.
+
+    if (SourceMaps.length === 0) {
+      return {
+        score: 1,
+        notApplicable: true,
+      };
     }
 
     /** @type {LH.Audit.Details.Table['headings']} */
     const headings = [
-      /* eslint-disable max-len */
-      {key: 'scriptUrl', itemType: 'url', subRows: {key: 'errors', itemType: 'text'}, text: str_(i18n.UIStrings.columnURL)},
+      {key: 'scriptUrl', itemType: 'url', text: str_(i18n.UIStrings.columnURL)},
       {key: 'sourceMapUrl', itemType: 'url', text: 'Map URL'}, // TODO uistring
-      /* eslint-enable max-len */
+      {key: 'error', itemType: 'code', text: 'Error'}, // TODO uistring
     ];
 
     results.sort((a, b) => {
-      // Show the items that can fail the audit first.
-      let missingMapA = isMissingMapForLargeFirstPartyScriptUrl.has(a.scriptUrl);
-      let missingMapB = isMissingMapForLargeFirstPartyScriptUrl.has(b.scriptUrl);
-      if (missingMapA && !missingMapB) return -1;
-      if (!missingMapA && missingMapB) return 1;
-
-      // Then sort by number of errors.
-      if (a.errors.length && !b.errors.length) return -1;
-      if (!a.errors.length && b.errors.length) return 1;
-
-      // Then sort by script url.
+      if (a.error && !b.error) return -1;
+      if (!a.error && b.error) return 1;
       return b.scriptUrl.localeCompare(a.scriptUrl);
     });
-
-    // Only fails if `missingMapsForLargeFirstPartyFile` is true. All other errors
-    // are diagnostical.
+    const passed = !results.find(result => result.error);
     return {
-      score: missingMapsForLargeFirstPartyFile ? 0 : 1,
+      score: Number(passed),
       details: Audit.makeTableDetails(headings, results),
     };
   }
